a4ca0ddbec49b1b12f8368bdcef527b5e66b1d25
dense77 <764925453@qq.com>
2024-11-13 16:03:22 +0800

    提交

--- src/test/test.java
+++ src/test/test.java
@@ -1,13 +1,17 @@
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class test {
 
-    public static void main(String[] args) throws Exception {
-//        Myclass myclass = new Myclass(1,"lyg","123456");
-//        System.out.println(myclass.toString());
-
-        Demo demo = new Demo(1);
-        System.out.println(demo.age);
+    public static void main(String[] args) {
+        float price1 = 10.9f;
+        double price2 = 5.8;
+        int num1 = 2;
+        int num2 = 4;
+        double res1 = price1 * num1 + price2 * num2;
+        int res2 = (int) (price1 * num1 + price2 * num2);
+        System.out.println("一共付给收银员" + res2 + "元");
+        System.out.println(res1);
     }
 }
 
