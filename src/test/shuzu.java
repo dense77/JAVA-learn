3b436372a02bbf0a53371e0ae4822346fbd0d9f9
dense77 <764925453@qq.com>
2024-11-21 11:26:51 +0800

    提交

--- src/test/shuzu.java
+++ src/test/shuzu.java
@@ -1,2 +1,46 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class shuzu {
+    public static void main(String[] args) {
+//        int[] anArray = new int[] {1, 7, 3, 8, 5};
+//        List<Integer> aList = new ArrayList<>();
+//        for (int element : anArray) {
+//            aList.add(element);
+//        }
+//        List<Integer> aList1 = Arrays.stream(anArray).boxed().collect(Collectors.toList());
+//        System.out.println(aList);
+//        System.out.println(aList1);
+
+//        List<Integer> aList2 = Arrays.asList(1, 7, 3, 8, 5);
+//        System.out.println(aList2);
+//
+//        Arrays.sort(anArray);
+//
+//        for(int i:anArray){
+//            System.out.println(i);
+//        }
+//
+//        String[] yetAnotherArray = new String[] {"A", "E", "Z", "B", "C"};
+//        Arrays.sort(yetAnotherArray, 1, 3, Comparator.comparing(String::toString).reversed());
+
+//        int[] anArray = new int[] {5, 2, 1, 4, 8};
+//        for (int i = 0; i < anArray.length; i++) {
+//            if (anArray[i] == 4) {
+//                System.out.println("找到了 " + i);
+//                break;
+//            }
+//        }
+//
+//
+//        int[] anArray1 = new int[] {1, 2, 3, 3, 4, 8};
+//        int index = Arrays.binarySearch(anArray1, 8);
+//
+//        System.out.println(index);
+
+
+    }
 }
